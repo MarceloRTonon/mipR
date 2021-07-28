f57d77ddb5756962907c7e99857fff6eb7cc46b2
MarceloRTonon <marceloresendetonon@gmail.com>
2021-07-28 10:00:57 -0300

    Top4

--- Topicos_em_Desenvolvimento/tamanho_dados.R
+++ Topicos_em_Desenvolvimento/tamanho_dados.R
@@ -1,12 +1,22 @@
-library(magrittr)
+library(purrr)
 
-bin
+checkSizeDifference <- function(nsize, benchmark = "lgl"){
+  asTypes <- list("lgl" = "as.logical",
+                  "int" = "as.integer",
+                  "dbl" = "as.double")
 
-cLogical <- c(TRUE, FALSE) %>%
-  rep( 1000) %>%
-  matrix(ncol=10)
+  nsample <- sample(c(T,F), nsize^2, replace=T)
+  
+ TypeSizes <-  asTypes %>%
+    map(function(x) call(x, nsample)) %>%
+    map(eval) %>%
+    map(matrix, nrow =nsize, ncol =nsize) %>%
+    map(object.size)
+ 
+ map(TypeSizes,
+     function(x) list("abs" = format(x, units="auto"),
+                       "relative" = as.double(x)/as.double(TypeSizes[[1]]))
+ )
+}
 
-cDouble <- rep(c(1,0), 1000) %>%
-  matrix(ncol =10)
-
-cInteger <- rep(c(1L, 0L, 1000))
+checkSizeDifference(nsize = 1000)  
